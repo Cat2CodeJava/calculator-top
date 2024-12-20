7d14751f0fa89bad12e4e56a97e022ff54940e94
Cat2CodeJava <157812791+Cat2CodeJava@users.noreply.github.com>
2024-12-20 15:28:16 +0800

    Add needed functions

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+let a = 1;
+let op = "+";
+let b = 2;
+
+
+function operate(a, b, op) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "ERROR";
+    }
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
